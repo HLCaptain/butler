8ed57969b71479ff143182e5a716169db955ab1c
HLCaptain <pkblazsak@gmail.com>
2024-01-13 22:24:10 +0100

    Formatting.

--- api_gateway/src/main/kotlin/ai/nest/api_gateway/data/service/ChatService.kt
+++ api_gateway/src/main/kotlin/ai/nest/api_gateway/data/service/ChatService.kt
@@ -13,7 +13,6 @@ import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.util.Attributes
 import io.ktor.utils.io.InternalAPI
-import kotlinx.coroutines.flow.Flow
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.protobuf.ProtoBuf
 import org.koin.core.annotation.Single
@@ -25,47 +24,46 @@ class ChatService(
     private val errorHandler: ErrorHandler
 ) {
     @OptIn(InternalAPI::class, ExperimentalSerializationApi::class)
-    suspend fun createTicket(ticket: TicketDto, language: String): TicketDto {
-        return client.tryToExecute<TicketDto>(
-            api = APIs.CHAT_API,
-            attributes = attributes,
-            setErrorMessage = { errorCodes ->
-                errorHandler.getLocalizedErrorMessage(errorCodes, language)
-            }
-        ) {
-            post("/chat/ticket") {
-                body = ProtoBuf.encodeToByteArray(TicketDto.serializer(), ticket)
-            }
+    suspend fun createTicket(
+        ticket: TicketDto,
+        languageCode: String
+    ) = client.tryToExecute<TicketDto>(
+        api = APIs.CHAT_API,
+        attributes = attributes,
+        setErrorMessage = { errorHandler.getLocalizedErrorMessage(it, languageCode) }
+    ) {
+        post("/chat/ticket") {
+            body = ProtoBuf.encodeToByteArray(TicketDto.serializer(), ticket)
         }
     }
 
-    suspend fun updateTicketState(ticketId: String, state: Boolean, language: String): Boolean {
-        return client.tryToExecute<Boolean>(
-            api = APIs.CHAT_API,
-            attributes = attributes,
-            setErrorMessage = { errorCodes ->
-                errorHandler.getLocalizedErrorMessage(errorCodes, language)
-            }
-        ) {
-            put("/chat/$ticketId") {
-                parameter("state", state)
-            }
+    suspend fun updateTicketState(
+        ticketId: String,
+        state: Boolean,
+        languageCode: String
+    ) = client.tryToExecute<Boolean>(
+        api = APIs.CHAT_API,
+        attributes = attributes,
+        setErrorMessage = { errorHandler.getLocalizedErrorMessage(it, languageCode) }
+    ) {
+        put("/chat/$ticketId") {
+            parameter("state", state)
         }
     }
 
-    suspend fun receiveTicket(supportId: String): Flow<TicketDto> {
-        return client.tryToExecuteWebSocket<TicketDto>(
-            api = APIs.CHAT_API,
-            attributes = attributes,
-            path = "/chat/tickets/$supportId",
-        )
-    }
-    suspend fun sendAndReceiveMessage(message: MessageDto, chatId: String): Flow<MessageDto> {
-        return client.tryToSendAndReceiveWebSocketData(
-            data = message,
-            api = APIs.CHAT_API,
-            attributes = attributes,
-            path = "/chat/$chatId"
-        )
-    }
+    suspend fun receiveTicket(supportId: String) = client.tryToExecuteWebSocket<TicketDto>(
+        api = APIs.CHAT_API,
+        attributes = attributes,
+        path = "/chat/tickets/$supportId",
+    )
+
+    suspend fun sendAndReceiveMessage(
+        message: MessageDto,
+        chatId: String
+    ) = client.tryToSendAndReceiveWebSocketData(
+        data = message,
+        api = APIs.CHAT_API,
+        attributes = attributes,
+        path = "/chat/$chatId"
+    )
 }
