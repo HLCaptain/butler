c18eb12004cb22e32fd1e38f46578f931fd52257
HLCaptain <pkblazsak@gmail.com>
2024-02-21 17:55:23 +0100

    Removed "jvm" appended to dependencies.

--- services/chat_service/Butler Chat Service/build.gradle.kts
+++ services/chat_service/Butler Chat Service/build.gradle.kts
@@ -40,29 +40,29 @@ repositories {
 
 dependencies {
     // Ktor Core
-    implementation(libs.ktor.server.core.jvm)
-    implementation(libs.ktor.server.websockets.jvm)
-    implementation(libs.ktor.server.content.negotiation.jvm)
-    implementation(libs.ktor.serialization.kotlinx.json.jvm)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.websockets)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.postgresql)
     implementation(libs.h2)
     implementation(libs.exposed.core)
     implementation(libs.exposed.jdbc)
     implementation(libs.exposed.crypt)
     implementation(libs.exposed.dao)
-    implementation(libs.ktor.server.swagger.jvm)
+    implementation(libs.ktor.server.swagger)
     implementation(libs.ktor.server.openapi)
-    implementation(libs.ktor.server.call.logging.jvm)
-    implementation(libs.ktor.server.metrics.jvm)
-    implementation(libs.ktor.server.call.id.jvm)
-    implementation(libs.ktor.server.metrics.micrometer.jvm)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.metrics)
+    implementation(libs.ktor.server.call.id)
+    implementation(libs.ktor.server.metrics.micrometer)
     implementation(libs.micrometer.registry.prometheus)
-    implementation(libs.ktor.server.compression.jvm)
-    implementation(libs.ktor.server.netty.jvm)
+    implementation(libs.ktor.server.compression)
+    implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
 
     // Tests
-    testImplementation(libs.ktor.server.tests.jvm)
+    testImplementation(libs.ktor.server.tests)
     testImplementation(libs.kotlin.test.junit)
 }
 
