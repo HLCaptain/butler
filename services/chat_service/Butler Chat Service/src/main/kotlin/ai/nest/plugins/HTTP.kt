92ce15426ad755c060733d78fbb99e6b265a72c0
HLCaptain <pkblazsak@gmail.com>
2024-02-21 16:17:52 +0100

    Initial commit of chat service.

---
+++ services/chat_service/Butler Chat Service/src/main/kotlin/ai/nest/plugins/HTTP.kt
@@ -0,0 +1,25 @@
+package ai.nest.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.routing.*
+
+fun Application.configureHTTP() {
+    routing {
+        swaggerUI(path = "openapi")
+    }
+    routing {
+        openAPI(path = "openapi")
+    }
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+}
